396980c3b9f07fca8c7d5ac3ead49e6df14917ad
BrayanAlexander-Reyes <bryanreyes47k@gmail.com>
2026-05-29 16:14:01 -0500

    se ajusta codigo de settigns para el api de tutoriales

--- schema_tutoriales/backend/settings.py
+++ schema_tutoriales/backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+debug = config('DEBUG', cast=bool)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # APP para usar el api rest
+    'rest_framework',
+    # APP PARA USAR SWAGGER
+    'drf_yasg',
+    # APP DEL API
+    'api_tutoriales',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +83,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'),
+        'PORT':config('DB_PORT'),
+        'OPTIONS': {
+            'options': f'-c search_path={config("DB_SCHEMA")}'
+        }
     }
 }
 
