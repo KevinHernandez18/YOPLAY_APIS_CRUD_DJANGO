2767b6123449ba5e36e8e7f6050a6f7f45ad052b
BrayanAlexander-Reyes <bryanreyes47k@gmail.com>
2026-06-01 14:50:56 -0500

    se genera cambios en los schemas de las apis para generar swagger

--- schema_usuario/backend/urls.py
+++ schema_usuario/backend/urls.py
@@ -17,7 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Usuario",
+        default_version='v1',
+        description="Documentación de la API de Usuario",
+        contact=openapi.Contact(email="admin@usuario.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('schema_usuario.urls')),
+    # Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+    name='schema-swagger-ui'
+),
 ]
--- schema_usuario/schema_usuario/urls.py
+++ schema_usuario/schema_usuario/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+# from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     DocumentoViewSet,
@@ -6,21 +6,19 @@ from .views import (
     ContrasenaViewSet,
     HistorialAccesoViewSet
 )
-from django.contrib import admin
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from django.contrib import admin
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
 router = DefaultRouter()
 
 # registro de los endpoints del api
 
-router.register(r'documento', DocumentoViewSet)
-router.register(r"usuario",UsuarioViewSet)
-router.register(r'contrasena',ContrasenaViewSet)
-router.register(r'historial_acceso',HistorialAccesoViewSet)
+router.register(r'documento', DocumentoViewSet, basename='documento')
+router.register(r"usuario",UsuarioViewSet, basename='usuario')
+router.register(r'contrasena',ContrasenaViewSet, basename='contrasena')
+router.register(r'historial_acceso',HistorialAccesoViewSet, basename='historial_acceso')
 
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = router.urls
